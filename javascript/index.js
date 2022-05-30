afe8b1a12f3ff7c4755f5b3894e66b1ebbe0d2c8
Anh Phan <teoanhss113@gmail.com>
2022-05-31 01:29:31 +0700

    Profile Modal

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,17 @@
-const inputWithdrawAmountToWithdraw = document.getElementById(
-  'inputWithdrawAmountToWithdraw'
+const inputUserNewPassword = document.getElementById('inputUserNewPassword')
+const inputUserNewPasswordConfirm = document.getElementById(
+  'inputUserNewPasswordConfirm'
 )
-
+const inputTransferCardNumber = document.getElementById(
+  'inputTransferCardNumber'
+)
+const inputTransferName = document.getElementById('inputTransferName')
+const inputWithdrawAmount = document.getElementById('inputWithdrawAmount')
 const inputWithdrawFee = document.getElementById('inputWithdrawFee')
-const alertWithdrawAmountToWithdraw = document.getElementById(
-  'alertWithdrawAmountToWithdraw'
-)
 
 const inputWithdrawTotalAmount = document.getElementById(
   'inputWithdrawTotalAmount'
 )
-
 const btnNextToTransferConfirm = document.getElementById(
   'btnNextToTransferConfirm'
 )
@@ -19,10 +20,19 @@ const btnNextToDepositConfirm = document.getElementById(
   'btnNextToDepositConfirm'
 )
 const btnPreviousToDeposit = document.getElementById('btnPreviousToDeposit')
+const btnNextToWithdrawConfirm = document.getElementById(
+  'btnNextToWithdrawConfirm'
+)
+const btnPreviousToWithdraw = document.getElementById('btnPreviousToWithdraw')
 const btnNextToPhoneCardConfirm = document.getElementById(
   'btnNextToPhoneCardConfirm'
 )
 const btnPreviousToPhoneCard = document.getElementById('btnPreviousToPhoneCard')
+
+const alertUserChangePassword = document.getElementById(
+  'alertUserChangePassword'
+)
+const alertWithdrawAmount = document.getElementById('alertWithdrawAmount')
 
 let modelTransfer = document.getElementById('modelTransfer')
 let modelTransferConfirm = document.getElementById('modelTransferConfirm')
@@ -33,35 +43,27 @@ let modelWithdrawConfirm = document.getElementById('modelWithdrawConfirm')
 let modelPhoneCard = document.getElementById('modelPhoneCard')
 let modelPhoneCardConfirm = document.getElementById('modelPhoneCardConfirm')
 
-const inputTransferCardNumber = document.getElementById(
-  'inputTransferCardNumber'
-)
-const inputTransferName = document.getElementById('inputTransferName')
-
-inputWithdrawAmountToWithdraw.addEventListener('keyup', function (event) {
-  event.preventDefault()
-  // inputWithdrawAmountToWithdraw divide by 50000
-  const inputWithdrawAmountToWithdrawValue = parseInt(
-    inputWithdrawAmountToWithdraw.value
-  )
-  if (inputWithdrawAmountToWithdrawValue % 50000 === 0) {
-    alertWithdrawAmountToWithdraw.classList.add('d-none')
+inputUserNewPasswordConfirm.addEventListener('keyup', function () {
+  if (inputUserNewPassword.value != inputUserNewPasswordConfirm.value) {
+    alertUserChangePassword.classList.remove('d-none')
   } else {
-    alertWithdrawAmountToWithdraw.classList.remove('d-none')
+    alertUserChangePassword.classList.add('d-none')
   }
-  console.log(inputWithdrawAmountToWithdrawValue * 0.05)
-  inputWithdrawFee.value = parseInt(inputWithdrawAmountToWithdrawValue) * 0.05
+})
+
+inputWithdrawAmount.addEventListener('keyup', function (event) {
+  event.preventDefault()
+  // inputWithdrawAmount divide by 50000
+  const inputWithdrawAmountValue = parseInt(inputWithdrawAmount.value)
+  if (inputWithdrawAmountValue % 50000 === 0) {
+    alertWithdrawAmount.classList.add('d-none')
+  } else {
+    alertWithdrawAmount.classList.remove('d-none')
+  }
+  console.log(inputWithdrawAmountValue * 0.05)
+  inputWithdrawFee.value = parseInt(inputWithdrawAmountValue) * 0.05
   inputWithdrawTotalAmount.value =
-    parseInt(inputWithdrawFee.value) +
-    parseInt(inputWithdrawAmountToWithdrawValue)
-})
-
-modelTransfer = new bootstrap.Modal(modelTransfer, {
-  keyboard: false,
-})
-
-modelTransferConfirm = new bootstrap.Modal(modelTransferConfirm, {
-  keyboard: false,
+    parseInt(inputWithdrawFee.value) + parseInt(inputWithdrawAmountValue)
 })
 
 modelDeposit = new bootstrap.Modal(modelDeposit, {
@@ -69,6 +71,22 @@ modelDeposit = new bootstrap.Modal(modelDeposit, {
 })
 
 modelDepositConfirm = new bootstrap.Modal(modelDepositConfirm, {
+  keyboard: false,
+})
+
+modelWithdraw = new bootstrap.Modal(modelWithdraw, {
+  keyboard: false,
+})
+
+modelWithdrawConfirm = new bootstrap.Modal(modelWithdrawConfirm, {
+  keyboard: false,
+})
+
+modelTransfer = new bootstrap.Modal(modelTransfer, {
+  keyboard: false,
+})
+
+modelTransferConfirm = new bootstrap.Modal(modelTransferConfirm, {
   keyboard: false,
 })
 
@@ -161,6 +179,64 @@ btnPreviousToDeposit.addEventListener('click', function (event) {
   event.preventDefault()
   modelDeposit.show()
   modelDepositConfirm.hide()
+})
+
+btnNextToWithdrawConfirm.addEventListener('click', function (event) {
+  event.preventDefault()
+  modelWithdraw.hide()
+  modelWithdrawConfirm.show()
+
+  const inputWithdrawCardNumber = document.getElementById(
+    'inputWithdrawCardNumber'
+  )
+  const inputWithdrawExpirationDate = document.getElementById(
+    'inputWithdrawCardExpirationDate'
+  )
+  const inputWithdrawCodeCVV = document.getElementById(
+    'inputWithdrawCardCodeCVV'
+  )
+  const inputWithdrawNote = document.getElementById('inputWithdrawNote')
+  const inputWithdrawAmount = document.getElementById('inputWithdrawAmount')
+  const inputWithdrawFee = document.getElementById('inputWithdrawFee')
+  const inputWithdrawTotalAmount = document.getElementById(
+    'inputWithdrawTotalAmount'
+  )
+
+  const inputWithdrawConfirmCardNumber = document.getElementById(
+    'inputWithdrawConfirmCardNumber'
+  )
+  const inputWithdrawConfirmExpirationDate = document.getElementById(
+    'inputWithdrawConfirmCardExpirationDate'
+  )
+  const inputWithdrawConfirmCodeCVV = document.getElementById(
+    'inputWithdrawConfirmCardCodeCVV'
+  )
+  const inputWithdrawConfirmNote = document.getElementById(
+    'inputWithdrawConfirmNote'
+  )
+  const inputWithdrawConfirmAmount = document.getElementById(
+    'inputWithdrawConfirmAmount'
+  )
+  const inputWithdrawConfirmFee = document.getElementById(
+    'inputWithdrawConfirmFee'
+  )
+  const inputWithdrawConfirmTotalAmount = document.getElementById(
+    'inputWithdrawConfirmTotalAmount'
+  )
+
+  inputWithdrawConfirmCardNumber.value = inputWithdrawCardNumber.value
+  inputWithdrawConfirmExpirationDate.value = inputWithdrawExpirationDate.value
+  inputWithdrawConfirmCodeCVV.value = inputWithdrawCodeCVV.value
+  inputWithdrawConfirmNote.value = inputWithdrawNote.value
+  inputWithdrawConfirmAmount.value = inputWithdrawAmount.value
+  inputWithdrawConfirmFee.value = inputWithdrawFee.value
+  inputWithdrawConfirmTotalAmount.value = inputWithdrawTotalAmount.value
+})
+
+btnPreviousToWithdraw.addEventListener('click', function (event) {
+  event.preventDefault()
+  modelWithdraw.show()
+  modelWithdrawConfirm.hide()
 })
 
 btnNextToPhoneCardConfirm.addEventListener('click', function (event) {
